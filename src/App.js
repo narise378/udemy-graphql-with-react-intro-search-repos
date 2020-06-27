09cfcc0a9671ba7a22115779df6b86bbd7ea14be
narise378 <narise378@gmail.com>
2020-06-27 22:25:23 +0900

    implement next button

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { Query } from 'react-apollo'
 import client from './client'
 import { SEARCH_REPOSITORIES } from './grapql'
 
+const PER_PAGE = 5
 const DEFAULT_STATE = {
-  first: 5,
+  first: PER_PAGE,
   after: null,
   last: null,
   before: null,
@@ -29,6 +30,15 @@ class App extends Component {
 
   handleSubmit(event) {
     event.preventDefault()
+  }
+
+  goNext(search) {
+    this.setState({
+      first: PER_PAGE,
+      after: search.pageInfo.endCursor,
+      last: null,
+      before: null,
+    })
   }
 
   render() {
@@ -63,13 +73,20 @@ class App extends Component {
                     const node = edge.node
                     return (
                       <li key={node.id}>
-                        <a href={node.url} target="__blank">
+                        <a
+                          href={node.url}
+                          target="__blank"
+                          rel="noopener noreferrer"
+                        >
                           {node.name}
                         </a>
                       </li>
                     )
                   })}
                 </ul>
+                {search.pageInfo.hasNextPage === true ? (
+                  <button onClick={this.goNext.bind(this, search)}>Next</button>
+                ) : null}
               </React.Fragment>
             )
           }}
